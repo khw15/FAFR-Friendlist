1f952b03e2ff487bd85366c8f43520535bcd3a0e
Faisal A F Rahman <64533786+khw15@users.noreply.github.com>
2024-06-28 22:11:14 +0700

    feat: Add two new friends to the FriendList component

--- components/FriendList.js
+++ components/FriendList.js
@@ -5,7 +5,8 @@ import FriendItem from './FriendItem';
 const friendsData = [
     { id: '1', name: 'Haewon', phone: '010-123-123', description: 'Haewon is leader of NMIXX', image: require('../assets/images/haewon.jpg')},
     { id: '2', name: 'Bae', phone: '010-123-123', description: 'Bae is vocal of NMIXX', image: require('../assets/images/bae.jpg')},
-    { id: '3', name: 'Saiz', phone: '0812-1234-1234', description: 'Saiz is a certified racist driver', image: require('../assets/images/saiz.jpg')},
+    { id: '3', name: 'Faiz Kicau Mania', phone: '0812-1234-1234', description: 'Faiz is a certified racist driver', image: require('../assets/images/saiz.jpg')},
+    { id: '4', name: 'Fauzan Erdin Saputra', phone: '0812-1234-1234', description: 'Fauzan is a certified racist reserve driver', image: require('../assets/images/fauzan.jpg')},
 ]
 
 const FriendList = ({ navigation }) => {
